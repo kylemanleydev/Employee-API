8c7a9e09a13a1b07b236be38ca5bdd8351dee4ee
supermyle <kylemanley99@gmail.com>
2022-11-14 08:04:53 -0500

    Added DbContext, started Migration, added mock connection string

---
+++ Data/Employee_CoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Employee_Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Core.Data
+{
+    public class Employee_CoreDbContext : DbContext
+    {
+        public Employee_CoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Employee_Core.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Employee_CoreDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDbConnectionString")));
 
 var app = builder.Build();
 

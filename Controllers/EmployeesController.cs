c4a6f2dcfd55ca8b78cc4655d0c61dd358030b5b
supermyle <kylemanley99@gmail.com>
2022-11-15 06:55:31 -0500

    Created Employee Controller

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,37 @@
+﻿using Employee_Core.Data;
+using Employee_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeDbContext _employeeDbContext;
+
+        public EmployeesController(EmployeeDbContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _employeeDbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+            await _employeeDbContext.Employees.AddAsync(employeeRequest);
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+        }
+    }
+}

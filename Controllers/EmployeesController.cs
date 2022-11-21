314410ab7407157f618a45699f5255b9e86bb37e
supermyle <kylemanley99@gmail.com>
2022-11-20 23:07:27 -0500

    Add UpdateEmployee function based on id

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -47,5 +47,27 @@ namespace Employee_Core.Controllers
 
             return Ok(employee);
         }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee updateEmployeeRequest)
+        {
+            var employee = await _employeeDbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Department = updateEmployeeRequest.Department;
+
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
     }
 }
